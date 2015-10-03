4854c6313f507639f0a1711e667f8fdf5b65e769
Alexander Heemann <alexanderheemann@googlemail.com>
2015-10-03 13:25:27 +0200

    Add method to initialize new character

--- Character.h
+++ Character.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface Character : NSManagedObject
 
 // Insert code here to declare functionality of your managed object subclass
+- (void)setupDefault;
 
 @end
 
--- Character.m
+++ Character.m
@@ -8,8 +8,26 @@
 
 #import "Character.h"
 
+#define START_SPECIAL_POINTS 21
+
 @implementation Character
 
 // Insert code here to add functionality to your managed object subclass
+
+- (void)setupDefault
+{
+    self.specialPoints = @(START_SPECIAL_POINTS);
+    self.dateCreated = [NSDate date];
+    self.lastUsed = [NSDate date];
+    self.strength = @(0);
+    self.perception = @(0);
+    self.endurance = @(0);
+    self.charisma = @(0);
+    self.intelligence = @(0);
+    self.agility = @(0);
+    self.luck = @(0);
+    self.perkPoints = @(0);
+    self.level = @(1);
+}
 
 @end
