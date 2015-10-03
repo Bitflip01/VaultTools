62b538a996493500ab22cf79959ba748dc23801c
Alexander Heemann <alexanderheemann@googlemail.com>
2015-10-03 13:26:56 +0200

    Setup data model in AppDelegate

--- Vault 111/AppDelegate.m
+++ Vault 111/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "CharacterManager.h"
 
 @interface AppDelegate ()
 
@@ -15,8 +16,11 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [MagicalRecord setupAutoMigratingCoreDataStack];
+    [[CharacterManager sharedCharacterManager] loadLastCharacterOrCreateNew];
+    
     return YES;
 }
 
@@ -38,8 +42,10 @@
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
+- (void)applicationWillTerminate:(UIApplication *)application
+{
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [MagicalRecord cleanUp];
 }
 
 @end
