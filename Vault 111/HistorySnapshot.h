2271cd3fda5ce3ad6c5ae7374d3600e3a6416827
Alexander Heemann <alexanderheemann@googlemail.com>
2015-10-22 13:48:04 +0200

    Add HistorySnapshot class

---
+++ Vault 111/HistorySnapshot.h
@@ -0,0 +1,17 @@
+//
+//  HistorySnapshot.h
+//  Vault Tools
+//
+//  Created by Alexander Heemann on 22/10/15.
+//  Copyright © 2015 Alexander Heemann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "StatsSnapshot.h"
+
+@interface HistorySnapshot : NSObject
+
+@property (nonatomic, strong, readwrite) StatsSnapshot *snapshot;
+@property (nonatomic, strong, readwrite) NSDictionary *changes;
+
+@end
---
+++ Vault 111/HistorySnapshot.m
@@ -0,0 +1,13 @@
+//
+//  HistorySnapshot.m
+//  Vault Tools
+//
+//  Created by Alexander Heemann on 22/10/15.
+//  Copyright © 2015 Alexander Heemann. All rights reserved.
+//
+
+#import "HistorySnapshot.h"
+
+@implementation HistorySnapshot
+
+@end
